2e31b3055ba58fe04027f92cb3861821fae66eef
ngoc minh <ttngocminh214@gmail.com>
2021-09-21 09:49:41 +0700

    a

--- src/rendering.js
+++ src/rendering.js
@@ -181,8 +181,8 @@ export default function link(scope, elem, attrs, ctrl) {
     var mouseover = function(d) {
       var position = d3.mouse(d3.select('#sunburst-div-' + ctrl.panel.id).node());
       var tooltipData = prepareTooltip(d, position);
-      var position_ = d3.mouse(d3.select('#sunburst-div-' + ctrl.panel.id));
-      console.log(position_);
+      //var position_ =d3. mouse(d3.select);
+      console.log($('#sunburst-div-' + ctrl.panel.id));
       updateTooltip(tooltipData);
     };
 
