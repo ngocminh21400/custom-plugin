7aa89e0c2d7f7a472c0eb74a4722a84b763c18b2
ngoc minh <ttngocminh214@gmail.com>
2021-09-22 09:52:04 +0700

    no message

--- src/rendering.js
+++ src/rendering.js
@@ -255,7 +255,7 @@ export default function link(scope, elem, attrs, ctrl) {
     var text = svg.selectAll("path")
       .append('text');
     var textPath = text.select('text')
-      .data([1,2,3])
+      .data(ctrl.data)
       .enter()
       .append('textPath')
       .attr("id",d=>d)
