bd8db7f6135b49a6ad30d0d1fc69d739b7804d7e
ngoc minh <ttngocminh214@gmail.com>
2021-09-22 09:24:48 +0700

    d

--- src/rendering.js
+++ src/rendering.js
@@ -253,9 +253,10 @@ export default function link(scope, elem, attrs, ctrl) {
       .on("mouseout", mouseout);
     
     var text = svg.selectAll("path")
-      .data(partition.nodes(hierarchy))
-      .enter()
-      .append(content);
+      // .data(partition.nodes(hierarchy))
+      // .enter()
+      .append('text');
+      
     console.log(text);
     
 
