e972e0e8f6ba6f03a901bc5b312d07f2872af138
ngoc minh <ttngocminh214@gmail.com>
2021-09-23 09:09:01 +0700

    no message

--- src/rendering.js
+++ src/rendering.js
@@ -540,7 +540,7 @@ export default function link(scope, elem, attrs, ctrl) {
   }
 
   function buildFixedTooltip(data) {
-    console.log(data);
+    //console.log(data);
     var tooltip = d3.select("#sunburst-legend-" + ctrl.panel.id)
       .style("left", "0px")
       .style("bottom", "0px")
