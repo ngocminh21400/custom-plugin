cf26197ab23390c453dff7beeddd9e05b4ba1665
ngoc minh <ttngocminh214@gmail.com>
2021-09-27 12:11:53 +0700

    no message

--- src/rendering.js
+++ src/rendering.js
@@ -288,7 +288,7 @@ export default function link(scope, elem, attrs, ctrl) {
       //console.log("-----------------------");
       //var x = (width/2) * d.y;
       //var y = (d.y + d.dy) / 2 * radius;
-      var r =  ((height + margin.top + margin.bottom) / 2)* 0.9; 
+      var r =  ((height + margin.top + margin.bottom) / 2) * 0.9  * (d.y + d.dy); 
 
       //console.log(r);
      // console.log( height + margin.top + margin.bottom);
@@ -306,7 +306,7 @@ export default function link(scope, elem, attrs, ctrl) {
       console.log("sin: " + Math.sin(angle));
       console.log("cos: " + Math.cos(angle));
       console.log("r: "+r);
-      var x = (-1) * Math.cos(angle) * r * (d.y + d.dy);
+      var x = (-1) * Math.cos(angle) * r;
       //console.log(x);
       //console.log((d.y + d.dy) * 2 * Math.PI);
       var y = (-1) * Math.sin(angle) * r;
